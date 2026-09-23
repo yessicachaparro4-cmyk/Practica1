43e1ad170b459bcbd8a722c69e35f79c00a1fcc0
yessicachaparro4-cmyk <yessicachaparro4@gmail.com>
2026-09-22 21:31:42 -0600

    Agrega calculadora básica en JavaScript

---
+++ calculadora-js/calculadora.js
@@ -0,0 +1,15 @@
+let nombre = prompt("¿Cómo te llamas?");
+
+let numero1 = Number(prompt("Escribe el primer número"));
+let numero2 = Number(prompt("Escribe el segundo número"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Hola " + nombre + ", estos son tus resultados:");
+console.log("Suma: " + suma);
+console.log("Resta: " + resta);
+console.log("Multiplicación: " + multiplicacion);
+console.log("División: " + division);
